0ebc437f3e0e12262af1715be5e71b56b8e6d6c1
SATHISH <sathishkumarr@nanonino.in>
2025-07-31 16:00:22 +0530

    remove static data removed

--- src/view/HomeSections/HomeScreen.tsx
+++ src/view/HomeSections/HomeScreen.tsx
@@ -18,29 +18,6 @@ import { ProductListResponseModel } from "../../model/apiResponseModel/ProductLi
 
 const windowWidth = Dimensions.get("window").width
 
-const products = [
-  { id: "1", name: "Samsung Galaxy S23", image: "https://placehold.co/150x150?text=Galaxy+S23", price: 799 },
-  { id: "2", name: "iPhone 14 Pro", image: "https://placehold.co/150x150?text=iPhone+14+Pro", price: 999 },
-  { id: "3", name: "Redmi Note 12", image: "https://placehold.co/150x150?text=Redmi+Note+12", price: 249 },
-  { id: "4", name: "OnePlus 11R", image: "https://placehold.co/150x150?text=OnePlus+11R", price: 429 },
-  { id: "5", name: "Realme Narzo 60", image: "https://placehold.co/150x150?text=Narzo+60", price: 199 },
-  { id: "6", name: "Vivo V27 Pro", image: "https://placehold.co/150x150?text=Vivo+V27+Pro", price: 379 },
-  { id: "7", name: "iQOO Z7", image: "https://placehold.co/150x150?text=iQOO+Z7", price: 239 },
-  { id: "8", name: "Motorola Edge 40", image: "https://placehold.co/150x150?text=Edge+40", price: 429 },
-  { id: "9", name: "Poco X5 Pro", image: "https://placehold.co/150x150?text=Poco+X5+Pro", price: 319 },
-  { id: "10", name: "Lava Blaze 5G", image: "https://placehold.co/150x150?text=Lava+Blaze+5G", price: 149 },
-  { id: "11", name: "Samsung M14 5G", image: "https://placehold.co/150x150?text=Samsung+M14+5G", price: 199 },
-  { id: "12", name: "Apple iPhone SE 2022", image: "https://placehold.co/150x150?text=iPhone+SE+2022", price: 429 },
-  { id: "13", name: "Nokia G42 5G", image: "https://placehold.co/150x150?text=Nokia+G42+5G", price: 159 },
-  { id: "14", name: "Infinix Zero 5G", image: "https://placehold.co/150x150?text=Infinix+Zero+5G", price: 189 },
-  { id: "15", name: "Micromax IN Note 2", image: "https://placehold.co/150x150?text=Micromax+Note+2", price: 179 },
-  { id: "16", name: "Asus ROG Phone 6", image: "https://placehold.co/150x150?text=ROG+Phone+6", price: 899 },
-  { id: "17", name: "Google Pixel 7a", image: "https://placehold.co/150x150?text=Pixel+7a", price: 499 },
-  { id: "18", name: "Nothing Phone 2", image: "https://placehold.co/150x150?text=Nothing+Phone+2", price: 599 },
-  { id: "19", name: "Tecno Camon 20", image: "https://placehold.co/150x150?text=Camon+20", price: 169 },
-  { id: "20", name: "Honor 90 5G", image: "https://placehold.co/150x150?text=Honor+90+5G", price: 349 },
-]
-
 type Props = {
   item : ProductListResponseModel
 }
